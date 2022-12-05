39549d32e6bcd992e46c691e726368b9c5d5b387
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-12-04 21:11:27 -0800

    feat: add category filters

--- scripts.js
+++ scripts.js
@@ -67,6 +67,8 @@ $(document).ready(() => {
         return accum;
       }, {});
 
+      const categories = Object.keys(categoryMentors);
+
       // Loop through every category and write mentors to the page
       Object.entries(categoryMentors)
         .sort((a, b) => a[1].length < b[1].length)
@@ -96,5 +98,56 @@ $(document).ready(() => {
             $(`li#${categoryId}`).append(getMentorRow(mentor));
           });
         });
+
+      // Initiate Select2 component
+      const multiSelectContainer = $("select#multiFilter");
+      multiSelectContainer.select2();
+
+      // Build options for multi select
+      categories.forEach((category) => {
+        multiSelectContainer.append(
+          `<option value="${getSerializedId(category)}">${category}</option>`
+        );
+      });
+
+      // Check and set category visibility
+      function updateCategoryVisibility(visibleBoxes) {
+        const categories = $(".category");
+
+        categories.each((index, categoryElement) => {
+          if (visibleBoxes.includes(categoryElement.id)) {
+            $(categoryElement).show();
+          } else {
+            $(categoryElement).hide();
+          }
+        });
+      }
+
+      // Listener to clear filters
+      $("button#multiFilterClear").on("click", () => {
+        multiSelectContainer.val(null).trigger("change");
+      });
+
+      // Reset visibility when clear invoke
+      multiSelectContainer.on("select2:clear", () => {
+        updateCategoryVisibility(
+          categories.map((category) => getSerializedId(category))
+        );
+      });
+
+      // Handler for change events, update visibility of categories
+      multiSelectContainer.on("change", (event) => {
+        const selectedCategories = multiSelectContainer
+          .select2("data")
+          .map((data) => getSerializedId(data.id));
+
+        if (selectedCategories.length === 0) {
+          updateCategoryVisibility(
+            categories.map((category) => getSerializedId(category))
+          );
+        } else {
+          updateCategoryVisibility(selectedCategories);
+        }
+      });
     });
 });
